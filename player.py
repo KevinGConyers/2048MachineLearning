40caf0555a5e6f563fb0339ba3e3ff735118971e
KevinGconyers <kevingconyers@gmail.com>
2019-04-23 10:56:47 -0500

    minor bugfix

--- player.py
+++ player.py
@@ -3,6 +3,10 @@ from logic import *
 import random
 from puzzle import *
 
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
 
 class Player():
     def __init__(self):
@@ -11,20 +15,45 @@ class Player():
         self.goal = 2048
 
     def play(self, board):
-        self.scan(board)
-        self.move(board)
+        move = self.scan(board)
+        self.move(board, move)
 
 
     def scan(self, board):
-        x=None
+        if board.matrix[0][0] == 0:
+            for i in range(4):
+                if board.matrix[0][i] != 0:
+                    return LEFT
+                if board.matrix[i][0]:
+                    return UP
+            for i in range(1, 3):
+                if board.matrix[i][0] != 0 and board.matrix[i][0] != board.matrix[0][0]:
+                    break
+                if board.matrix[0][i] != 0 and board.matrix[0][i] != board.matrix[0][0]:
+                    break
+                if board.matrix[0][0] == board.matrix[0][i]:
+                    return LEFT
+                if board.matrix[0][0] == board.matrix[i][0]:
+                    return UP
+        targeti = 0
+        targetj = 0
+        maxNum = board.matrix[0][0]
+        for i in range(4):
+            for j in range(4):
+                if i == 0 and j == 0:
+                    continue
+                if board.matrix[i][j] > maxNum:
+                    targeti = i
+                    targetj = j
 
-    def move(self, board):
-        x = randint(0,3)
-        if x == 0:
+        return randint(0, 3)
+
+    def move(self, board, move):
+        if move  == 0:
             board.bot_move(KEY_UP)
-        if x == 1:
+        if move == 1:
             board.bot_move(KEY_RIGHT)
-        if x == 2:
+        if move == 2:
             board.bot_move(KEY_DOWN)
-        if x==3:
+        if move == 3:
             board.bot_move(KEY_LEFT)
